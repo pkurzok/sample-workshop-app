7276f197f758a2fde198220b2c3771dd46dd3b19
Arnold Noronha <arnold@screenshotbot.io>
2026-02-09 09:28:04 -0500

    Initial Commit

---
+++ SampleWorkshopApp/SampleWorkshopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWorkshopAppApp.swift
+//  SampleWorkshopApp
+//
+//  Created by Arnold Noronha on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleWorkshopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleWorkshopAppTests/SampleWorkshopAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWorkshopAppTests.swift
+//  SampleWorkshopAppTests
+//
+//  Created by Arnold Noronha on 2/9/26.
+//
+
+import Testing
+@testable import SampleWorkshopApp
+
+struct SampleWorkshopAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
